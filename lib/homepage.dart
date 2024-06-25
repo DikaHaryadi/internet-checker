2b7051e831e11af5bb6516ed66adb8e54ab53cc5
DikaHaryadi <araydika2@gmail.com>
2024-06-25 12:18:09 +0700

    versi GetX

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,68 +1,38 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:get/get.dart';
+import 'package:internetchecker/internet_checker_controller.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  bool isConnectedToInternet = false;
-
-  StreamSubscription? _streamSubscription;
-
-  @override
-  void initState() {
-    _streamSubscription = InternetConnection().onStatusChange.listen((event) {
-      switch (event) {
-        case InternetStatus.connected:
-          setState(() {
-            isConnectedToInternet = true;
-          });
-          break;
-        case InternetStatus.disconnected:
-          setState(() {
-            isConnectedToInternet = false;
-          });
-        default:
-          setState(() {
-            isConnectedToInternet = false;
-          });
-          break;
-      }
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _streamSubscription?.cancel();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final InternetController internetController = Get.put(InternetController());
+
     return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.sizeOf(context).width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Icon(
-              isConnectedToInternet ? Icons.wifi : Icons.wifi_off,
-              size: 50,
-              color: isConnectedToInternet ? Colors.green : Colors.red,
-            ),
-            Text(isConnectedToInternet
-                ? 'Your are connected to the internet.'
-                : 'You are bot connected to the internet')
-          ],
-        ),
+      body: Center(
+        child: Obx(() {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                internetController.isConnectedToInternet.value
+                    ? Icons.wifi
+                    : Icons.wifi_off,
+                size: 50,
+                color: internetController.isConnectedToInternet.value
+                    ? Colors.green
+                    : Colors.red,
+              ),
+              Text(
+                internetController.isConnectedToInternet.value
+                    ? 'You are connected to the internet.'
+                    : 'You are not connected to the internet',
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
